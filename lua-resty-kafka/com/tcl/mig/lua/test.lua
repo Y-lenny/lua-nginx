d1601762355f7feebf88e69a9bd430feee2c0960
lennylv <lennylv@tcl.com>
2016-07-28 20:32:02 +0800

    update

---
+++ lua-resty-kafka/com/tcl/mig/lua/test.lua
@@ -0,0 +1,14 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lennylv
+-- Date: 2016/7/12
+-- Time: 12:15
+-- To change this template use File | Settings | File Templates.
+--
+local color={first="red", "blue", third="green", "yellow"}
+print(color["first"])                 --> output: red
+print(color[1])                       --> output: blue
+print(color["third"])                 --> output: green
+print(color[2])                       --> output: yellow
+print(color[3])                       --> output: nil
+
